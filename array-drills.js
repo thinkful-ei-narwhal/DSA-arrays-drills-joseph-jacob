c7255e0e473d346a1a3f930ce127c9d8a1eb2de8
antonibrivera <josephr98@gmail.com>
2020-06-10 12:10:06 -0400

    added memory modules in 'array-drils.js', completed adding 'push'

--- array-drills.js
+++ array-drills.js
@@ -1,22 +1,23 @@
 const Memory = require('./memory');
+const memory = new Memory();
 
 class Array {
   constructor() {
     this.length = 0;
-    this.ptr = Memory.allocate(this.length);
+    this.ptr = memory.allocate(this.length);
   }
 
   _resize(size) {
     const oldPtr = this.ptr;
     this.ptr = memory.allocate(size);
     if (this.ptr === null) throw new Error('Out of Memory')
-    Memory.copy(this.ptr, oldPtr, this.length);
-    Memory.free(oldPtr);
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
   }
 
   push(value) {
     this._resize(this.length + 1);
-    Memory.set(this.ptr + this.length, value);
+    memory.set(this.ptr + this.length, value);
     this.length++;
   }
 }
@@ -27,6 +28,8 @@ function main() {
   let arr = new Array();
 
   arr.push(3);
+  arr.push(2);
+  arr.push(10);
 
   console.log(arr);
 }
